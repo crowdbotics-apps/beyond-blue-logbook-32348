10d237ef49ef08c3ab680ab955c969586ef6e3da
Crowdbotics <team@crowdbotics.com>
2021-12-11 04:14:57 +0000

    Update models

--- backend/beyond_blue_logbook_32348/wsgi.py
+++ backend/beyond_blue_logbook_32348/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beyond_blue_logbook_32348.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beyond_blue_logbook_32348.settings")
 
 application = get_wsgi_application()
